8a02fbae221cebb564fd3debd0c6f417f69ffe02
petervitale910 <petervitale910@gmail.com>
2025-08-26 19:04:15 +0000

    added graph

--- scratch/spaghetti.R
+++ scratch/spaghetti.R
@@ -1,24 +1,20 @@
-pacman::p_load("tidyverse","janitor","here", "zoo", "slider", "forecast")
+pacman::p_load("tidyverse","janitor","here", "zoo", "patchwork", "ggthemes")
 
 q1dat <- read_csv(here::here("data","QuebradaCuenca1-Bisley.csv")) %>% 
   clean_names() %>% 
-  select(c("sample_id", "sample_date", "k","nh4_n","no3_n","mg","ca")) %>%
-  na.omit()
+  select(c("sample_id", "sample_date", "k","nh4_n","no3_n","mg","ca")) 
 
 q2dat <- read_csv(here::here("data","QuebradaCuenca2-Bisley.csv")) %>% 
   clean_names() %>% 
-  select(c("sample_id", "sample_date", "k","nh4_n","no3_n","mg","ca")) %>%
-  na.omit()
+  select(c("sample_id", "sample_date", "k","nh4_n","no3_n","mg","ca")) 
 
 q3dat <- read_csv(here::here("data","QuebradaCuenca3-Bisley.csv")) %>% 
   clean_names() %>% 
-  select(c("sample_id", "sample_date", "k","nh4_n","no3_n","mg","ca")) %>%
-  na.omit()
+  select(c("sample_id", "sample_date", "k","nh4_n","no3_n","mg","ca")) 
 
 mprdat <- read_csv(here::here("data","RioMameyesPuenteRoto.csv")) %>% 
   clean_names() %>% 
-  select(c("sample_id", "sample_date", "k","nh4_n","no3_n","mg","ca")) %>%
-  na.omit()
+  select(c("sample_id", "sample_date", "k","nh4_n","no3_n","mg","ca")) 
 
 
 allsitedf <- rbind(mprdat, q1dat, q2dat, q3dat)
@@ -43,8 +39,34 @@ average_allsite <- allsitedf %>%
          mg_mean = rollmean(mg, k=9, fill = NA),
          ca_mean = rollmean(ca, k=9, fill = NA)
   ) %>% 
-  na.omit() %>% 
-  filter(year(sample_date) <= 1998) %>% 
-  select(-c(potassium, nh4_n, no3_n, mg, ca)) 
+  filter(year(sample_date) <= 1994) %>% 
+  select(-c(potassium, nh4_n, no3_n, mg, ca))
 
+k <- ggplot(average_allsite, aes(x = sample_date, 
+                                 y = potassium_mean))+
+              geom_line(aes(color = sample_id))+
+  theme_few()
 
+no3 <- ggplot(average_allsite, aes(x = sample_date, 
+                                 y = no3_n_mean))+
+  geom_line(aes(color = sample_id))+
+  theme_few()
+
+nh4 <- ggplot(average_allsite, aes(x = sample_date, 
+                                   y = nh4_mean))+
+  geom_line(aes(color = sample_id))+
+  theme_few()
+
+mg <-  ggplot(average_allsite, aes(x = sample_date, 
+                                   y = mg_mean))+
+  geom_line(aes(color = sample_id))+
+  theme(axis.text.x = element_blank(), legend.position = "none")+
+  theme_few()
+
+ca <- ggplot(average_allsite, aes(x = sample_date, 
+                                  y = ca_mean))+
+  geom_line(aes(color = sample_id))+
+  theme_few()
+
+k/no3/nh4/mg/ca
+  
