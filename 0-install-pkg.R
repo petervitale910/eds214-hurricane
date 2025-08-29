4b47a3a198e799848ceabcac135b9077b71c2ed4
petervitale910 <petervitale910@gmail.com>
2025-08-29 14:37:57 -0700

    fixed 20

---
+++ 0-install-pkg.R
@@ -0,0 +1,8 @@
+#In this script we will be downloading all the neccesary packages to run the code. 
+
+install.packages("tidyverse")
+install.packages("janitor")
+install.packages("here")
+install.packages("ggthemes")
+install.packages("patchwork")
+install.packages("ARTofR")
